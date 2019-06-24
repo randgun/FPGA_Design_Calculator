dc275ac63f15212baa709d50ccdbbd883e9045f9
randgun <45850286+randgun@users.noreply.github.com>
2019-06-24 16:19:23 +0800

    Update calculate.v

--- calculate.v
+++ calculate.v
@@ -8,11 +8,17 @@ module calculate(
 );
   reg a;
   reg b;
-  reg [23:0] key1;
-  reg [23:0] key2;
-  reg [31:0] sum;
+  reg [11:0] key1;
+  reg [11:0] key2;
+  reg [11:0] sum;
   reg [7:0] num;
+  reg [11:0] binary;
+  reg [3:0] Hundreds;
+  reg [3:0] Tens;
+  reg [3:0] Ones;
+  reg [11:0] hh;
 
+assign result = hh;
 always @(reg_num2)
 begin
   a<=0;
@@ -61,10 +67,36 @@ begin
   key2<=sum;
   //用a、b、c、d、e分别代表加减乘除和等于
   case (sym)
-    8'h61: result <= key1 + key2;
-    8'h62: result <= key1 - key2;
-    8'h63: result <= key1 * key2;
-    8'h64: result <= key1 / key2;
+    8'h61: binary <= key1 + key2;
+    8'h62: binary <= key1 - key2;
+    8'h63: binary <= key1 * key2;
+    8'h64: binary <= key1 / key2;
   endcase
 end
+
+  always@(binary)
+  begin
+  //set 100's,10's,and 1'8 too
+    Hundreds=4'd0;
+    Tens=4'd0;
+    Ones=4'd0;
+    for (i=7;i>=0;i=i-1)
+      begin
+      //add 3 to columns>=5
+        if(Hundreds>=5)
+          Hundreds=Hundreds+3;
+        if(Tens>=5)
+          Tens=Tens+3:
+        if(Ones>=5)
+          Ones=Ones+3;
+        //shift left one
+        Hundreds=Hundreds << 1;
+        Hundreds[0]=Tens[3];
+        Tens=Tens << 1;
+        Tens[0]=Ones[3])
+        Ones=Ones <<1;
+        Ones[0]=binary[i];
+      end
+    hh = {Hundreds,Tens,Ones};
+  end
 endmodule
