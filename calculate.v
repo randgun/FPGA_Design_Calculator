353fb80586f75b5c3a53d5dc5b8f5bf1b4c464bb
randgun <45850286+randgun@users.noreply.github.com>
2019-06-24 14:17:16 +0800

    Update calculate.v

--- calculate.v
+++ calculate.v
@@ -1,46 +1,70 @@
 module calculate(
-input p1,
-input p2,
-input sym,
-output [23:0] result
+  input reg_num1,
+  input reg_num2,
+  input sym,
+  input cnt1,
+  input cnt2,
+  output result
 );
-reg a;
-reg [7:0] key1;
-reg [7:0] key2;
+  reg a;
+  reg b;
+  reg [23:0] key1;
+  reg [23:0] key2;
+  reg [31:0] sum;
+  reg [7:0] num;
 
-always @(*)
+always @(reg_num2)
 begin
-  case (p1)
-    8'h30: key1 = 8'h00;    //0
-    8'h31: key1 = 8'h01;    //1
-    8'h32: key1 = 8'h02;    //2
-    8'h33: key1 = 8'h03;    //3
-    8'h34: key1 = 8'h04;    //4
-    8'h35: key1 = 8'h05;    //5
-    8'h36: key1 = 8'h06;    //6
-    8'h37: key1 = 8'h07;    //7
-    8'h38: key1 = 8'h08;    //8
-    8'h39: key1 = 8'h09;    //9
-  endcase
-  
-  case (p2)
-    8'h30: key2 = 8'h00;    //0
-    8'h31: key2 = 8'h01;    //1
-    8'h32: key2 = 8'h02;    //2
-    8'h33: key2 = 8'h03;    //3
-    8'h34: key2 = 8'h04;    //4
-    8'h35: key2 = 8'h05;    //5
-    8'h36: key2 = 8'h06;    //6
-    8'h37: key2 = 8'h07;    //7
-    8'h38: key2 = 8'h08;    //8
-    8'h39: key2 = 8'h09;    //9
-  endcase
+  a<=0;
+  sum<=0;
+  num<=0;
+  while(a<cnt1)
+    while(b<4)
+      case (b)
+        2'b0: num <= num + reg_num1[4*a+b]*1;
+        2'b1: num <= num + reg_num1[4*a+b]*2;
+        2'b2: num <= num + reg_num1[4*a+b]*4;
+        2'b3: num <= num + reg_num1[4*a+b]*8;
+      endcase
+      b<=b+1;
+    end
+    case (a)
+      2'b0: sum <= sum + num;
+      2'b1: sum <= sum + num*10;
+      2'b2: sum <= sum + num*100;
+      2'b3: sum <= sum + num*1000;
+    endcase
+    a<=a+1;
+  end
+  key1<=sum;
+  a<=0;
+  sum<=0;
+  num<=0;
+  while(a<cnt2)
+    while(b<4)
+      case (b)
+        2'b0: num <= num + reg_num2[4*a+b]*1;
+        2'b1: num <= num + reg_num2[4*a+b]*2;
+        2'b2: num <= num + reg_num2[4*a+b]*4;
+        2'b3: num <= num + reg_num2[4*a+b]*8;
+      endcase
+      b<=b+1;
+    end
+    case (a)
+      2'b0: sum <= sum + num;
+      2'b1: sum <= sum + num*10;
+      2'b2: sum <= sum + num*100;
+      2'b3: sum <= sum + num*1000;
+    endcase
+    a<=a+1;
+  end
+  key2<=sum;
   
   case (sym)
-    8'h2b: result = key1 + key2;
-    8'h2d: result = key1 - key2;
-    8'h2a: result = key1 * key2;
-    8'h2f: result = key1 / key2;
+    8'h61: result <= key1 + key2;
+    8'h62: result <= key1 - key2;
+    8'h63: result <= key1 * key2;
+    8'h64: result <= key1 / key2;
   endcase
 end
 endmodule
