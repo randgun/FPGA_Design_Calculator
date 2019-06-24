9b7a16a8af75068352516e89c0c0c6b223c269bd
randgun <45850286+randgun@users.noreply.github.com>
2019-06-24 09:44:11 +0800

    Update control.v

--- control.v
+++ control.v
@@ -1,29 +1,29 @@
 module in_ctrl(
 input CLK_1K,
 input RSTN,
-inpat [3:0]key_value,
+input [3:0] key_value,
 input flag,
-input [23:0]num result,
-output reg[23:0]num_regl,
-outpat reg[23:0]num _reg2,
-output reg[3:0]opcode,
-output [23:0]num out
+input [23:0] num_result,
+output reg[23:0] num_regl,
+outpat reg[23:0] num _reg2,
+output reg[3:0] opcode,
+output [23:0] num_out
 );
 
 localparam IDLE = 4'b000;
-localparam SO = 4'b001;
+localparam S0 = 4'b001;
 localparam S1=4'b010;
 localparam S2=4'b100;
-assign num_out=num reg2 ? num reg2:num regl;
+assign num_out = num reg2 ? num reg2:num regl;
 reg [3:0] state_now,state_nxt;
 reg [3:0] opcode_reg;
 
 always@(posedge CLK_1K or negedge RSTN)
 begin
 if(~RSTN)
-  state now <= S0;
+  state_now <= S0;
 else
-  state now <= state _nxt;
+  state_now <= state_nxt;
 end
 
 always@(posedge CLK_1K or negedge RSTN)
@@ -40,7 +40,7 @@ begin
     case(state_now)
     
     SO:if((key_value >= 4'h0)&&(key_value <= 4'h9))
-        num_reg1 <= {num reg1[19:0],key value};
+        num_reg1 <= {num_reg1[19:0],key_value};
       else if(key_value>=4'ha && key_value <= 4'hd)
         begin
           state_nxt<=S1;
