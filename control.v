bc902c7b5182077d3554909f3708beb6c841a878
randgun <45850286+randgun@users.noreply.github.com>
2019-06-26 11:57:15 +0800

    Update control.v

--- control.v
+++ control.v
@@ -7,20 +7,24 @@ input [23:0] num_result,
 output reg[23:0] num_reg1,
 output reg[23:0] num_reg2,
 output reg[3:0] opcode,
-output [23:0] num_out
+output [23:0] num_out,
 output cnt1,
 output cnt2,
 output state_nxt
 );
-
 localparam IDLE = 4'b000;
 localparam S0 = 4'b001;
 localparam S1 = 4'b010;
 localparam S2 = 4'b100;
 assign num_out = num_reg2 ? num_reg2 : num_reg1;
-reg [3:0] state_now,state_nxt;
+reg [3:0] state_now, next;
 reg [3:0] opcode_reg;
 reg delay;
+reg m1;
+reg m2;
+assign cnt1 = m1;
+assign cnt2 = m2;
+assign state_nxt = next;
 
 always@(posedge CLK_1K or negedge RST)
 begin
@@ -30,7 +34,7 @@ else
   state_now <= state_nxt;
 end
 
-always@(posedge CLK_1K or negedge RSTN)
+always@(posedge CLK_1K or negedge RST)
 begin
   if(~RST)
     begin
@@ -38,8 +42,8 @@ begin
       num_reg2 <= 0;
       opcode_reg <= 0;
       opcode <= 0;
-      cnt1 = 0;
-      cnt2 = 0;
+      m1 = 0;
+      m2 = 0;
     end
   else if(flag)
    
@@ -48,28 +52,28 @@ begin
     S0:if((key_value >= 4'h0)&&(key_value <= 4'h9))
         begin
           num_reg1 <= {num_reg1[19:0],key_value};
-          cnt1 <= cnt1 + 1;
+          m1 <= m1 + 1;
         end
       else if(key_value>=4'ha && key_value <= 4'hd)
         begin
-          state_nxt<=S1;
+          next<=S1;
           opcode_reg <= key_value;
         end
       
-    S1:if(key_value >= 4'ha && key value<= 4'hd)
+    S1:if(key_value >= 4'ha && key_value<= 4'hd)
         opcode_reg <= key_value;
       else if(key_value >= 4'h0 && key_value <= 4'h9)
         begin
-          num_reg2 <= {num_reg2(19:0],key_value[3:0]};
+          num_reg2 <= {num_reg2[19:0],key_value[3:0]};
           opcode<=opcode_reg;
-          cnt2 <= cnt2 + 1;
+          m2 <= m2 + 1;
         end
       else if(key_value == 4'he)
         begin
          
        //   num_reg2 <= 0;
           opcode_reg <= 0;
-          state_nxt <= S2;
+          next <= S2;
           delay = 20;
           while(delay)
             delay = delay - 1;
@@ -78,11 +82,11 @@ begin
                                 
     S2:if(key_value>=4'h0 && key_value<=4'h9)
         begin
-          cnt1 <= 1;
-          cnt2 <= 0;
+          m1 <= 1;
+          m2 <= 0;
           num_reg2 <= 0;
           num_reg1 <= key_value;
-          state_nxt <= S0;
+          next <= S0;
         end
    //     else if((key_value >= 4'ha) && (key_value <= 4'hd))
    //       begin
