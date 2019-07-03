11f07d59612f2236262930ecac5cb74535fc1f68
randgun <45850286+randgun@users.noreply.github.com>
2019-07-03 22:18:57 +0800

    Update calculate.v

--- calculate.v
+++ calculate.v
@@ -1,9 +1,10 @@
 module calculate(
-  input reg_num1,
-  input reg_num2,
+  input [11:0]reg_num1,
+  input [11:0]reg_num2,
   input sym,
-  input cnt1,
-  input cnt2,
+  //input cnt1,
+  //input cnt2,
+  input clk,
   output result
 );
   reg a;
@@ -11,7 +12,7 @@ module calculate(
   reg [11:0] key1;
   reg [11:0] key2;
   reg [11:0] sum;
-  reg [7:0] num;
+  //reg [7:0] num;
   reg [11:0] binary;
   reg [3:0] Hundreds;
   reg [3:0] Tens;
@@ -20,11 +21,16 @@ module calculate(
   wire [11:0] result; 
 
 assign result = hh;
-always @(reg_num2)
+always @(clk)
 begin
-  a<=0;
+  //a<=0;
   sum<=0;
-  num<=0;
+  //num<=0;
+  sum <= sum + reg_num1[11:8]
+  sum <= sum * 10;
+  sum <= (sum + reg_num1[7:4])*10;
+  sum <= sum + reg_num1[3:0];
+  /*
   while(a<cnt1)
     while(b<4)
       case (b)
@@ -43,11 +49,15 @@ begin
     endcase
     a<=a+1;
   end
+  */
   key1<=sum;
-  a<=0;
   sum<=0;
-  num<=0;
-  while(a<cnt2)
+  sum <= sum + reg_num2[11:8]
+  sum <= sum * 10;
+  sum <= (sum + reg_num2[7:4])*10;
+  sum <= sum + reg_num2[3:0];
+  //num<=0;
+  /*while(a<cnt2)
     while(b<4)
       case (b)
         2'b0: num <= num + reg_num2[4*a+b]*1;
@@ -65,6 +75,7 @@ begin
     endcase
     a<=a+1;
   end
+  */
   key2<=sum;
   //用a、b、c、d、e分别代表加减乘除和等于
   case (sym)
@@ -75,7 +86,7 @@ begin
   endcase
 end
 
-  always@(binary)
+always@(binary)
   begin
   //set 100's,10's,and 1's to 0
     Hundreds=4'd0;
@@ -84,20 +95,20 @@ end
     for (i=7;i>=0;i=i-1)
       begin
       //add 3 to columns>=5
-        if(Hundreds>=5)
-          Hundreds=Hundreds+3;
-        if(Tens>=5)
+        if(Hundreds >= 5)
+          Hundreds = Hundreds + 3;
+        if(Tens >= 5)
           Tens=Tens+3:
-        if(Ones>=5)
-          Ones=Ones+3;
+          if(Ones >= 5)
+          Ones = Ones+3;
         //shift left one
-        Hundreds=Hundreds << 1;
-        Hundreds[0]=Tens[3];
-        Tens=Tens << 1;
-        Tens[0]=Ones[3];
-        Ones=Ones <<1;
-        Ones[0]=binary[i];
+        Hundreds = Hundreds << 1;
+        Hundreds[0] = Tens[3];
+        Tens = Tens << 1;
+        Tens[0] = Ones[3];
+        Ones = Ones <<1;
+        Ones[0] = binary[i];
       end
     hh = {Hundreds,Tens,Ones};
-  end
+end
 endmodule
