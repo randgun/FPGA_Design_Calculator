8878b5f495a0bc59ecebc2c55df13377df585a4d
randgun <45850286+randgun@users.noreply.github.com>
2019-07-03 22:53:15 +0800

    Update control.v

--- control.v
+++ control.v
@@ -8,8 +8,8 @@ output reg[23:0] num_reg1,
 output reg[23:0] num_reg2,
 output reg[3:0] opcode,
 output [23:0] num_out,
-output cnt1,
-output cnt2,
+//output cnt1,
+//output cnt2,
 output state_nxt
 );
 localparam IDLE = 4'b000;
@@ -20,10 +20,10 @@ assign num_out = num_reg2 ? num_reg2 : num_reg1;
 reg [3:0] state_now, next;
 reg [3:0] opcode_reg;
 reg delay;
-reg m1;
-reg m2;
-assign cnt1 = m1;
-assign cnt2 = m2;
+//reg m1;
+//reg m2;
+//assign cnt1 = m1;
+//assign cnt2 = m2;
 assign state_nxt = next;
 
 always@(posedge CLK_1K or negedge RST)
@@ -42,8 +42,8 @@ begin
       num_reg2 <= 0;
       opcode_reg <= 0;
       opcode <= 0;
-      m1 = 0;
-      m2 = 0;
+    //  m1 = 0;
+   //   m2 = 0;
     end
   else if(flag)
    
@@ -52,7 +52,7 @@ begin
     S0:if((key_value >= 4'h0)&&(key_value <= 4'h9))
         begin
           num_reg1 <= {num_reg1[19:0],key_value};
-          m1 <= m1 + 1;
+       //   m1 <= m1 + 1;
         end
       else if(key_value>=4'ha && key_value <= 4'hd)
         begin
@@ -66,7 +66,7 @@ begin
         begin
           num_reg2 <= {num_reg2[19:0],key_value[3:0]};
           opcode<=opcode_reg;
-          m2 <= m2 + 1;
+         // m2 <= m2 + 1;
         end
       else if(key_value == 4'he)
         begin
@@ -82,8 +82,8 @@ begin
                                 
     S2:if(key_value>=4'h0 && key_value<=4'h9)
         begin
-          m1 <= 1;
-          m2 <= 0;
+       //   m1 <= 1;
+       //   m2 <= 0;
           num_reg2 <= 0;
           num_reg1 <= key_value;
           next <= S0;
