a5c19cb85052ae28056db68e984c8a5c1a04f5b7
randgun <45850286+randgun@users.noreply.github.com>
2019-07-07 16:08:03 +0800

    Update top.v

--- top.v
+++ top.v
@@ -1,97 +1,104 @@
-module seg_clock(clock, ps2_clk, ps2_data, rst, seg_d, seg_w); 
+module seg_clock(clock, rst, seg_d, seg_w,w_key,val); 
 input  clock;     //系统时钟50MHZ晶振输入
-input rst;
+input  rst;
+input [3:0] val;
+input [4:0] w_key;
 output seg_d;     //数码管的段选
 output seg_w;     //数码管的位选
 
 wire[7:0] seg_d;  //段选8位
 wire[3:0] seg_w;  //位选4位
-reg [3:0] key;
-reg flag;
-wire   clk1;      //分频后的时钟1 因为分频器输出为reg类型，故这里用wire
-wire   clk2;      //同上
-wire [23:0] num_reg1,
-wire [23:0] num_reg2,
+wire [3:0] key;
+
+wire   clk2;      //500HZ时钟
+wire [11:0] num_reg1;
+wire [11:0] num_reg2;
 wire [3:0] opcode;
 wire [11:0] num_out;
-wire cnt1;
-wire cnt2;
-wire [3:0] state_nxt;
-wire [11:0] result;
-wire [11:0] code;
-wire [3:0] ps2_byte;
+wire [7:0] result;
+wire [23:0] my_code;
+wire [3:0] byte;
+wire [11:0] p1;
+wire [11:0] p2;
+wire state;
+wire a;
 
-initial   //初始化数据
- begin
-  flag = 0;
-  
- end
+reg cnt;
+	always @(posedge clock or negedge rst) begin
+		if (!rst) 
+			cnt <= 1'b0;
+		else 
+			cnt <= cnt + 1'b1;
+	end
+	
+	wire clk_50m;
+	assign clk_50m = cnt;
 
 ////////////实例化分频模块////////////////
 clock_div   clock_div(
-                       .clk(clock),
-		       .clk_out1(clk1),
-		       .clk_out2(clk2)
+                       .clk(clk_50m),
+		         
+		               .clk_out2(clk2)
 		     );
 
 ////////////实例化ps2模块////////////////
-PS2scan    PS2scan(
+the_key    the_key(
                     .clk(clock),
-	            .rst_n(rst),
-	            .ps2_clk(ps2_clk),
-	            .ps2_data(ps2_data),
-	            .ps2_byte(ps2_byte)
+	                .rst(rst),
+	                .val(val),
+	                .w_key(w_key),
+	                .byte(byte),
+	                .state(state),
+	                .a(a)
                   );
 
-////////////实例化数据转换模块////////////	
-trans   trans(
-                .ps2_byte(ps2_byte),
-                .key_value(key)
-
-             );
+bcd  bcd2 (
+             .b(result),
+             .p(p2)
+);
 
 ////////////实例化控制模块///////////////
 in_ctrl control(
                   .CLK_1K(clock),
                   .RST(rst),
-                  .key_value(key),
-                  .flag(flag),
-                  .num_result(result),
-                  .num_reg1(num_reg1),
-                  .num_reg2(num_reg2),
-                  .opcode(opcode),
+                  .key_value(byte),
+                 // .flag(flag),
+                  .num_result(p2),
+                  .reg1(num_reg1),
+                  .reg2(num_reg2),
+                  .opcodedd(opcode),
                   .num_out(num_out),
-                  .cnt1(cnt1),
-                  .cnt2(cnt2),
-                  .state_nxt(state_nxt)
+                  .flag(state)
                 );
  
 ////////////实例化计算模块///////////////
 calculate calculate(
-                     .reg_num1(num_reg1),
-                     .reg_num2(num_reg2),
+                     .num_reg1(num_reg1),
+                     .num_reg2(num_reg2),
+                     .rst(rst),
                      .sym(opcode),
-                     .cnt1(cnt1),
-                     .cnt2(cnt2),
+                     .clk(clock),
+                     .eval_flag(a),
                      .result(result)
 
 );
 
 ////////////实例化段码转换模块////////////						 
-to_code  num_to_code(
-                      .data(num_out),
-		      .code(code)
+num_to_code  to_code(  
+                      .my_code(my_code),
+                      .my_data(num_out),
+                      .rst(rst)
+		              
 		    );
 
 ////////////实例化显示模块////////////////								
-Display   display(
+display   Display(
                    .clk(clk2),
-		   .seg(code),		      
-		   .seg_d(seg_d),
+                   .rst(rst),
+		               .seg(my_code),		      
+		               .seg_d(seg_d),
                    .seg_w(seg_w)
 		 );		
 
-always@(ps2_byte)
-  flag <= 1;
-
 endmodule
+
